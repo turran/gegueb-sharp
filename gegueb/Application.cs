34bc0810257a16a7d522013cf99d77e82ce2974e
Jorge Zapata <jorgeluis.zapata@gmail.com>
2016-02-10 21:28:42 +0100

    Initial commit

---
+++ gegueb/Application.cs
@@ -0,0 +1,57 @@
+namespace Gegueb {
+
+	using System;
+	using Egueb.Dom;
+
+	public class Application {
+
+		//
+		// Disables creation of instances.
+		//
+		private Application ()
+		{
+		}
+
+		[DllImport("libglib-2.0-0.dll")]
+		static extern void g_main_loop_run ();
+
+		[DllImport("libglib-2.0-0.dll")]
+		static extern void g_main_loop_quit ();
+
+		[DllImport("libgegueb.dll")]
+		static extern void gegueb_init ();
+
+		[DllImport("libgegueb.dll")]
+		static extern void gegueb_shutdown ();
+
+		[DllImport("libgegueb.dll")]
+		static extern IntPtr gegueb_window_new(IntPtr doc, int x, int y, int w, int h);
+
+		public static void Init ()
+		{
+			gegueb_init();
+		}
+
+		public static void Run ()
+		{
+			g_main_loop_run(loop);
+		}
+
+		public static void Quit ()
+		{
+			g_main_loop_quit(loop);
+		}
+
+		public Window New (Egueb.Dom.Document doc, int x, int y, int w, int h)
+		{
+			IntPtr wPtr = gegueb_window_new (doc.Raw, x, y, w, h);
+			if (wPtr != null)
+			{
+				Window w = new Window (wPtr);
+				return w;
+			}
+
+			return null;
+		}
+	}
+}
